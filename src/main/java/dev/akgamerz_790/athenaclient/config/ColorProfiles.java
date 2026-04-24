3692429e2ad15f98b2796ced2d7c3af9b0c77103
akgamerz790 <rockdgyt352@gmail.com>
2026-04-25 03:13:53 +0530

    Create ColorProfiles.java

---
+++ src/main/java/dev/akgamerz_790/athenaclient/config/ColorProfiles.java
@@ -0,0 +1,10 @@
+package dev.akgamerz_790.athenaclient.config;
+
+public class ColorProfiles{
+    public static final int COLOR_WHITE  = 0xFFFFFF;
+    public static final int COLOR_GREEN  = 0x00FF88;
+    public static final int COLOR_YELLOW = 0xFFAA00;
+    public static final int COLOR_CYAN   = 0x55FFFF;
+    public static final int COLOR_RED    = 0xFF5555;
+    public static final int COLOR_PURPLE = 0xAA55FF;
+}
